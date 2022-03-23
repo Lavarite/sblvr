93d2a2a67475174954a271639b84bc2b113854bc
Lavarite <102068981+Lavarite@users.noreply.github.com>
2022-03-23 03:51:54 +0300

    Add files via upload

---
+++ sblvr/src/main/java/me/lavarite/pluginsb/Items/Commands/ItemCommands.java
@@ -0,0 +1,48 @@
+package me.lavarite.pluginsb.Items.Commands;
+
+import me.lavarite.pluginsb.Items.ItemStorage.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+
+public class ItemCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("LOL IMAGINE DOING THIS!!1!");
+        }
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (cmd.getName().equalsIgnoreCase("giveItem")) {
+                if (args.length == 1) {
+                    switch (args[0].toLowerCase()) {
+                        case ("aspectofthe"): {
+                            player.getInventory().addItem(AOTE.ASPECT_OF_THE_END, AOTV.ASPECT_OF_THE_VOID, AOTM.MODIFIED_ASPECT_OF_THE_VOID);
+                        }
+                        case ("goldorset"): {
+                            player.getInventory().addItem(GoldorBoots.GOLDOR_BOOTS, GoldorChestplate.GOLDOR_CHEST, GoldorLeggings.GOLDOR_LEGS, GoldorBoots.GOLDOR_BOOTS);
+                        }
+                        case ("stormset"): {
+                            player.getInventory().addItem(StormHelmet.STORM_HELM, StormChestplate.STORM_CHEST, StormLeggings.STORM_LEGS, StormBoots.STORM_BOOTS);
+                        }
+                        case ("necronset"): {
+                            player.getInventory().addItem(NecronHelmet.NECRON_HELM, NecronChestplate.NECRON_CHEST, NecronLeggings.NECRON_LEGS, NecronBoots.NECRON_BOOTS);
+                        }
+                        case ("lordset"): {
+                            player.getInventory().addItem(LordHelmet.LORD_HELM, LordChestplate.LORD_CHEST, LordLeggings.LORD_LEGS, LordBoots.LORD_BOOTS);
+                        }
+                        default: {
+                            player.sendMessage("Don't be making up items now");
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ sblvr/src/main/java/me/lavarite/pluginsb/Items/ItemStorage/AIS.java
@@ -0,0 +1,27 @@
+package me.lavarite.pluginsb.Items.ItemStorage;
+
+public class AIS {
+    public static void ItemInit() {
+        HYPE.init();
+        AOTE.initAOTE();
+        AOTV.initAOTV();
+        AOTM.initAOTM();
+        GoldorHelmet.initGH();
+        GoldorChestplate.initGC();
+        GoldorLeggings.initGL();
+        GoldorBoots.initGB();
+        StormHelmet.initSH();
+        StormChestplate.initSC();
+        StormLeggings.initSL();
+        StormBoots.initSB();
+        NecronHelmet.initNH();
+        NecronChestplate.initNC();
+        NecronLeggings.initNL();
+        NecronBoots.initNB();
+        LordHelmet.initLH();
+        LordChestplate.initLC();
+        LordLeggings.initLL();
+        LordBoots.initLB();
+        strgDrag.initDrag();
+    }
+}
